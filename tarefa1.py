d74e7f6a65380e7584d303d271038663fa45a713
João Pereira <joaodper@gmail.com>
2021-07-20 13:15:24 +0100

    Create tarefa.py

---
+++ tarefa1.py
@@ -0,0 +1,7 @@
+def decode_float32(variável_array):
+  # descodifica os 4 octetos hexadecimal em float32
+  # devolve o valor descodificado
+  return decode_values
+
+array_to_decode = [0x3E, 0x99, 0x99, 0x9A]
+decode_float32(array_to_decode)
